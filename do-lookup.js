227da5f8ee17030a338d6b2b8abfc2c6b6c307ea
CJ <chad.dotsonjones@gmail.com>
2023-02-01 20:02:07 -0500

    Add some verbose comments to explain the process

--- do-lookup.js
+++ do-lookup.js
@@ -9,53 +9,75 @@
 const axios = require('axios');
 
 async function doLookup(entities) {
-    let entriesSearched = 0;
-    let entriesSkipped = 0;
-    let entriesErrored = 0;
+	// Initialize variables to keep track of the number of entries searched, skipped and errored
+	// These aren't used for anything, but are useful for debugging purposes
+	let entriesSearched = 0;
+	let entriesSkipped = 0;
+	let entriesErrored = 0;
   
-    const results = [];
-    const seen = new Set();
-    for (const entity of entities) {
-      if (entity.isIP && entity.type === "IPv4") {
-        if (seen.has(entity.value)) {
-          console.warn(`Skipping duplicated entry for ${entity.value}.`);
-          entriesSkipped += 1;
-          continue;
-        }
-        seen.add(entity.value);
-        entriesSearched += 1;
-        try {
-          const response = await axios.get(`https://internetdb.shodan.io/${entity.value}`);
-          results.push({
-            entity,
-            data: response.data,
-          });
-        } catch (error) {
-          entriesErrored += 1;
-          if (error.response && error.response.status === 404) {
-            console.warn(`No results found for ${entity.value}. Setting data to null.`);
-            results.push({
-              entity,
-              data: null,
-            });
-          } else {
-            console.error(`An error occurred while querying the Shodan API for ${entity.value}:`, error);
-          }
-        }
-      } else {
-        console.warn(`Skipping non-IPv4 entity: ${entity.value}`);
-        entriesSkipped += 1;
-      }
-    }
+	// Results array to store all the entries that have been searched
+	const results = [];
   
-    console.log(`Number of entries searched: ${entriesSearched}`);
-    console.log(`Number of entries skipped: ${entriesSkipped}`);
-    console.log(`Number of entries with errors: ${entriesErrored}`);
+	// Set to store all the entities that have been searched to avoid duplicates
+	const seen = new Set();
   
-    return results;
-  }
+	// Loop through all the entities that need to be searched
+	for (const entity of entities) {
+		// Check if the entity is an IP and it's of type IPv4
+		if (entity.isIP && entity.type === "IPv4") {
+			// Check if the entity has already been searched
+			if (seen.has(entity.value)) {
+				console.warn(`Skipping duplicated entry for ${entity.value}.`);
+		  		// Increment entriesSkipped as we are skipping this entry
+		  		entriesSkipped += 1;
+		  		continue;
+			}
+		
+			// Add the entity to the set of seen entities to avoid duplicates
+			seen.add(entity.value);
+			// Increment entriesSearched as we are searching this entry
+			entriesSearched += 1;
+			try {
+		  	// Use the Axios library to send a GET request to the Shodan API
+		  	const response = await axios.get(`https://internetdb.shodan.io/${entity.value}`);
+		  	// Push the entity and its response data to the results array
+		  	results.push({
+				entity,
+				data: response.data,
+		  	});
+			} catch (error) {
+		  		// Increment entriesErrored as an error occurred while searching this entry
+		  		entriesErrored += 1;
+		  		// Check if the error is a 404 error (no results found)
+		  		if (error.response && error.response.status === 404) {
+					console.warn(`No results found for ${entity.value}. Setting data to null.`);
+					// Push the entity and its response data (null) to the results array
+					results.push({
+			  			entity,
+			  			data: null,
+					});
+		  		} else {
+					console.error(`An error occurred while querying the Shodan API for ${entity.value}:`, error);
+		  		}
+			}
+	  	} else {
+			console.warn(`Skipping non-IPv4 entity: ${entity.value}`);
+
+			// Increment entriesSkipped as we are skipping this non-IPv4 entry
+			entriesSkipped += 1;
+	  	}
+	}
+  
+	// Log the number of entries that have been searched, skipped and errored
+	console.log(`Number of entries searched: ${entriesSearched}`);
+	console.log(`Number of entries skipped: ${entriesSkipped}`);
+	console.log(`Number of entries with errors: ${entriesErrored}`);
+  
+	// Return the results array
+	return Promise.resolve(results);
+}
   
 
 module.exports = {
-    doLookup
+	doLookup
 }
