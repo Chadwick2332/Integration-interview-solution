339d994e917713a27c93e069e563de767f86dfdf
CJ <chad.dotsonjones@gmail.com>
2023-02-01 19:39:25 -0500

    Added some verbose console.logs to the result

--- do-lookup.js
+++ do-lookup.js
@@ -9,37 +9,52 @@
 const axios = require('axios');
 
 async function doLookup(entities) {
+    let entriesSearched = 0;
+    let entriesSkipped = 0;
+    let entriesErrored = 0;
+  
     const results = [];
     const seen = new Set();
     for (const entity of entities) {
-        if (entity.isIP && entity.type === "IPv4") {
-
-            if (seen.has(entity.value)) {
-                console.warn(`Skipping duplicated entry for ${entity.value}.`);
-                continue;
-            }
-            seen.add(entity.value);
-            try {
-                const response = await axios.get(`https://internetdb.shodan.io/${entity.value}`);
-                results.push({
-                    entity,
-                    data: response.data,
-            });
-            } catch (error) {
-            if (error.response && error.response.status === 404) {
-                results.push({
-                    entity,
-                    data: null,
-                });
-            } else {
-                console.error(`An error occurred while querying the Shodan API for ${entity.value}:`, error);
-            }
+      if (entity.isIP && entity.type === "IPv4") {
+        if (seen.has(entity.value)) {
+          console.warn(`Skipping duplicated entry for ${entity.value}.`);
+          entriesSkipped += 1;
+          continue;
         }
+        seen.add(entity.value);
+        entriesSearched += 1;
+        try {
+          const response = await axios.get(`https://internetdb.shodan.io/${entity.value}`);
+          results.push({
+            entity,
+            data: response.data,
+          });
+        } catch (error) {
+          entriesErrored += 1;
+          if (error.response && error.response.status === 404) {
+            console.warn(`No results found for ${entity.value}. Setting data to null.`);
+            results.push({
+              entity,
+              data: null,
+            });
+          } else {
+            console.error(`An error occurred while querying the Shodan API for ${entity.value}:`, error);
+          }
+        }
+      } else {
+        console.warn(`Skipping non-IPv4 entity: ${entity.value}`);
+        entriesSkipped += 1;
+      }
     }
-}
-return Promise.resolve(results);
-}
-
+  
+    console.log(`Number of entries searched: ${entriesSearched}`);
+    console.log(`Number of entries skipped: ${entriesSkipped}`);
+    console.log(`Number of entries with errors: ${entriesErrored}`);
+  
+    return results;
+  }
+  
 
 module.exports = {
     doLookup
